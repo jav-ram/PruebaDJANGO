61217b619728c63c03ee7a95dfd1131215f747e0
Javier <ram16230@uvg.edu.gt>
2018-03-28 15:27:52 -0600

    borre todo lo que no estabamos usando

--- First/first/show/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse('<form action="/your-name/" method="post">{% csrf_token %}{{ form }}<input type="submit" value="Submit" /></form>')
--- JS/script/connect.js
+++
@@ -1,8 +0,0 @@
-function init() {
-	const pg = require('pg');
-	//"postgres://YourUserName:YourPassword@localhost:5432/YourDatabase";
-	var conString = "postgres://postgres:j66352769@localhost:5432/turismo";
-
-	var client = new pg.Client(conString);
-	client.connect();
-}
