41df5ddec456a24edd03483d60fdbd2d0772db65
Josrodjr <Josrodjr@gmail.com>
2018-04-02 19:20:40 -0600

    purgar todo lo de sql y dejar solo mongo

--- proyecto/app.js
+++ proyecto/app.js
@@ -143,7 +143,7 @@ app.get('/TwitterDisplay', function(req, resp, next) {
 
     var twtts = [];
     var usrs = [];
-
+/*
     client.query(query, (err, res) => {
     if (err) {
       console.log(err.stack)
@@ -155,43 +155,46 @@ app.get('/TwitterDisplay', function(req, resp, next) {
 
       for(let i = 0; i < respuesta.length; i++){
 
-        MongoClient.connect(url, function(err, db) {
-          if (err) throw err;
-          var dbo = db.db("twitter");
-          var query = {"user.screen_name" : respuesta[i].twitter.substr(1) };
-          dbo.collection("tweets").find(query).toArray(function(err, result) {
-            if (err) throw err;
-            //console.log(respuesta[i].twitter);
-            //como chingados parseo esto
-            //console.log(result[0].text);
-            //console.log(result[0].user.name);
-
-            for(let j = 0; j < 10; j++){
-
-              twtts.push(result[j].text);
-              //console.log(twtts[j]);
-              usrs.push(result[j].user.name);
-              //console.log(usrs[j]);
-
-            }
-
-            db.close();
-            //console.log(twtts);
-            //console.log(usrs);
-            resp.render('twuser', { tweets: twtts, users: usrs});
-          });
-
-        });
-
         //render the view
         //console.log('hola');
 
 
       }
 
+
+
     }
   })
+*/
 
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("twitter");
+        //var query = {"user.screen_name" : respuesta[i].twitter.substr(1) };
+        var query2 = { }
+        dbo.collection("tweets").find(query2).limit(25).sort( { "created_at" : 1 } ).toArray(function(err, result) {
+          if (err) throw err;
+          //console.log(respuesta[i].twitter);
+          //como chingados parseo esto
+          //console.log(result[0].text);
+          //console.log(result[0].user.name);
+
+          for(let j = 0; j < 25; j++){
+
+            twtts.push(result[j].text);
+            //console.log(twtts[j]);
+            usrs.push(result[j].user.name);
+            //console.log(usrs[j]);
+
+          }
+
+          db.close();
+          //console.log(twtts);
+          //console.log(usrs);
+          resp.render('twuser', { tweets: twtts, users: usrs});
+        });
+
+      });
   //falta: levantar un view que haga un query a la db y que para cada user saque los 10 tweets
 
 });
